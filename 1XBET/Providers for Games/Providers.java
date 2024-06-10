9b19e178562ad0fb5fba82800b8f15bacd558064
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-06-10 20:12:21 +0300

    Create Providers.java

---
+++ 1XBET/Providers for Games/Providers.java
@@ -0,0 +1,43 @@
+Playtech
+  Microgaming
+  Igt
+   Pragmatic Play
+    Red Tiger
+    Big Time Gamaing
+    PLayson
+    Sceientific Games
+    Ezugi
+    Saucify
+    Wager Gaming
+    Bally
+    PariPlay
+    Habanero
+    Rabcat
+    1X2 Gaming
+    Tom horn
+    Iron Dog Studio
+    MrSlotty
+    genii
+    just for the win
+    Buck Stakes Entertainmane
+    Old Skool Studios
+    Alchemy Gaming
+
+
+    Tripple Edges Studios
+    Crazy Tooth Studiosn
+    GONG Gaming Studios
+    Wizard Games
+    Spribe
+    All41Studios
+    Neko Games
+    GameBurgerSStudios
+    SnorBorn Games
+    Foxium
+    Reel Kingdom
+    Switch Studios
+    SoftGaming
+    Vivo Gaming
+    Spin Play Games
+    Slingo
+    Games Global
