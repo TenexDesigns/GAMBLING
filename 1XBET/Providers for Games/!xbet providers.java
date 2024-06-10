7594f592a0cbc9987ba21287d83a3da993af24f4
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-06-10 20:51:37 +0300

    Create !xbet providers.java

---
+++ 1XBET/Providers for Games/!xbet providers.java
@@ -0,0 +1,113 @@
+Mancala
+  Barbara gang
+    SmartSoft
+    Spinomanal
+    Winspinity
+    Gambit
+    7 mojos
+    MGA
+    Holle Games
+    High 5 games
+    3 oaks
+    playson
+    PG soft
+    Mascot gaming
+    KA gaming
+    NetGame
+    Eurasian gaming
+    Slot factory
+    pragmatic play
+    Simplay play
+    wazdan
+    Mrslotty
+    NGM game
+    Belatra
+    fazi
+    NetGaming
+    Mega &
+    Tripple PG
+    Aviatrix
+    Bet2tech
+    GameART
+    GamePlay
+    Ruby Play
+    geniio
+
+    onlyplay
+    netent
+    worldmathc
+    Genesisi
+    swintt
+    rival
+    Plystar
+    funku
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+---------------------------------
+SPIN OF THRONES
+
+NEW
+
+POPULAR
+
+EXCLUSIVE
+
+3D SLOTS
+
+BUY BONUS
+
+CLASSIC SLOTS
+
+CRASH
+
+DROPS & WINS
+
+HINDI STYLE
+
+HOLD AND WIN
+
+MEGAWAYS
+
+SKILL
+
+ASIAN GAMES
+
+BACCARAT
+
+BINGO
+
+BLACKJACK
+
+JACKPOT GAME
+
+OTHER
+
+CASCADE
+
+KENO
+
+CLUSTER
+
+"BOOK OF" SLOTS
+
+POKER
+
+ROULETT
