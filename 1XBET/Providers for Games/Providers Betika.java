a6f82d421f65b5f836842da8d38a942af0baa937
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-06-10 20:17:25 +0300

    Create Providers Betika.java

---
+++ 1XBET/Providers for Games/Providers Betika.java
@@ -0,0 +1,23 @@
+Playtech
+  IGT
+  Evolutuion Gaming
+    Pragmatic Play
+    Nektan
+    WMS
+    Evoplay
+    Wazdan
+    Habanero
+    Booming Games
+    Markor
+    Rising Enetratainamenet
+    Reel KingDom
+    Wild Streak Gamaiang
+    Intouch Games
+    Slot Factory
+    OneTouch
+    PlatNova
+    Gamezix
+    SimplePlay
+    Caleta
+    Spribe
+    3 oaks
