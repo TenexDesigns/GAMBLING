8a3f651396e1e994d18dcab1abb07f033770b442
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-06-11 06:18:15 +0300

    Update Games with better stratgey.java

--- Strategy Games/Games with better stratgey.java
+++ Strategy Games/Games with better stratgey.java
@@ -1,9 +1,16 @@
+video poker.
+Blackjack: 0.43% – 2%
+3	Baccarat	1.5%
+
+
+
+
+
 Roulette
 Baccarat
 Craps
 Blackjack
 Poker games vary wildly in how likely you are to win. 
-video poker.
 
 
 
@@ -11,11 +18,9 @@ video poker.
 
 1	Blackjack [using basic strategy]	0.5%
 2	Craps [certain bets]	1%
-3	Baccarat	1.5%
 4	European Roulette	2.7%
 
 Video Poker: -0.05% – 2%
-Blackjack: 0.43% – 2%
 Baccarat: 1.06% – 1.24%
 Craps: 1.36% – 1.41%
 Ultimate Texas Hold’em: 2.20%
